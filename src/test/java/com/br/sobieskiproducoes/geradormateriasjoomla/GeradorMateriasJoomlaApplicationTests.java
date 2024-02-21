7bd3f7bfb551d01f33c6c176bab9a1607befbd96
Jorge Demetrio Sobieski Teixeira <jorge.demetrio@alldreams.com.br>
2024-02-21 16:40:31 -0300

    Primeira versão

---
+++ src/test/java/com/br/sobieskiproducoes/geradormateriasjoomla/GeradorMateriasJoomlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.sobieskiproducoes.geradormateriasjoomla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorMateriasJoomlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
