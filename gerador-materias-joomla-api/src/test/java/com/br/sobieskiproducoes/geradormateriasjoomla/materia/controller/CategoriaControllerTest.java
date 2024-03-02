87b970e36d650a07f31c1b17547c45e3df5ca47b
Jorge Demetrio Sobieski Teixeira <jorge.demetrio@alldreams.com.br>
2024-03-02 00:09:33 -0300

    Correções para implementação da task #12
    Melhorando a performance de carga de mateiras

--- gerador-materias-joomla-api/src/test/java/com/br/sobieskiproducoes/geradormateriasjoomla/materia/controller/CategoriaControllerTest.java
+++ gerador-materias-joomla-api/src/test/java/com/br/sobieskiproducoes/geradormateriasjoomla/materia/controller/CategoriaControllerTest.java
@@ -15,10 +15,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.br.sobieskiproducoes.geradormateriasjoomla.config.properties.CargaDadosImagensProperties;
+import com.br.sobieskiproducoes.geradormateriasjoomla.config.properties.ChatGPTConfigurationProperties;
+import com.br.sobieskiproducoes.geradormateriasjoomla.config.properties.ConfiguracoesProperties;
+import com.br.sobieskiproducoes.geradormateriasjoomla.config.properties.DadosImagensLogoProperties;
+import com.br.sobieskiproducoes.geradormateriasjoomla.config.properties.DadosImagensMateriasProperties;
+import com.br.sobieskiproducoes.geradormateriasjoomla.config.properties.JoomlaConfigurationProperties;
+import com.br.sobieskiproducoes.geradormateriasjoomla.config.properties.PosicaoEnum;
 import com.br.sobieskiproducoes.geradormateriasjoomla.materia.service.CategoriaService;
 
 /**
@@ -26,7 +34,7 @@ import com.br.sobieskiproducoes.geradormateriasjoomla.materia.service.CategoriaS
  * @since 21 de fev. de 2024 20:11:34
  * @version 1.0.0
  */
-@ExtendWith({ MockitoExtension.class })
+@ExtendWith(MockitoExtension.class)
 class CategoriaControllerTest {
 
   @InjectMocks
@@ -34,6 +42,16 @@ class CategoriaControllerTest {
 
   @Mock
   CategoriaService service;
+
+  @Spy
+  ConfiguracoesProperties properties = new ConfiguracoesProperties(
+      new CargaDadosImagensProperties(
+          new DadosImagensMateriasProperties("pastaImagemMaterias", "autor", 1, 1, 2.4d, "constante", "url",
+              Boolean.TRUE),
+          new DadosImagensLogoProperties("path", PosicaoEnum.ALEATORIO, PosicaoEnum.ALEATORIO, 1, 1, 2.5d, 1, 2.4d)),
+      new ChatGPTConfigurationProperties("url", "bearer", "assistente", "organization", "model", 2.3d, "roleUser",
+          "roleSystem", "roleAssistant", "maxTokens"),
+      new JoomlaConfigurationProperties("url", "bearer", "idioma"));
 
   @Test
   void testAtualizar() {
