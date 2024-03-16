07ae35980a0038cbf9d934fcee8a6a7050619bf5
acarolinesb <159742893+acarolinesb@users.noreply.github.com>
2024-03-16 17:39:44 -0300

    Tarefa #33
    Feito o teste unit de apagar da #33 e rodando sem problemas.

--- gerador-materias-joomla-api/src/test/java/com/br/sobieskiproducoes/geradormateriasjoomla/materia/service/MateriaServiceTest.java
+++ gerador-materias-joomla-api/src/test/java/com/br/sobieskiproducoes/geradormateriasjoomla/materia/service/MateriaServiceTest.java
@@ -3,12 +3,47 @@
  */
 package com.br.sobieskiproducoes.geradormateriasjoomla.materia.service;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.br.sobieskiproducoes.geradormateriasjoomla.materia.model.MateriaEntity;
+import com.br.sobieskiproducoes.geradormateriasjoomla.materia.repository.MateriaRepository;
+import com.br.sobieskiproducoes.geradormateriasjoomla.materia.service.convert.TagConvert;
+
 /**
  * @author Ane Batista
  * @since 15 de mar. de 2024 15:45:00
  * @version 1.0.0
  * 
  */
-public class MateriaServiceTest {
 
+@ExtendWith(MockitoExtension.class)
+public class MateriaServiceTest {
+	
+	@InjectMocks
+	MateriaService service;
+	
+	@Mock
+	MateriaRepository repository;
+	
+	@Spy
+	TagConvert convert;
+	
+	@Test
+	void  apagarMateriaTest() {
+		final Long id = 10L;
+		final Optional<MateriaEntity> materiaEntityOpt = repository.findById(id);
+		when(repository.findById(id)).thenReturn(materiaEntityOpt);
+		assertFalse(service.apagar(id));
+		
+	}
 }
